7002f93afda6f88dd9b023517ad2be0ccb4e5fe3
Amrit <ehthind@gmail.com>
2016-07-20 16:05:46 -0700

    Added keyboard shortcuts to login/reg forms

--- public/js/form.js
+++ public/js/form.js
@@ -1,3 +1,18 @@
+/**
+ * Triggered on keyboard button press
+ *
+ * @param {object} event
+ */
+document.onkeydown = function (event) {
+
+    /**
+     * if the user pressed escape, remove the notification
+     */
+    if (event.key === "Enter") {
+        register();
+    }
+};
+
 function register() {
   var username = $("#user-name").val();
   var password = $("#password").val();
